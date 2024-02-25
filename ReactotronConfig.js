d957eed108cb73b71a640319d63b2e26a9282379
Chamara sanjeewa <chamara.sanjeewa@rootcodelabs.com>
2024-02-25 09:26:32 +0000

    wip: refactored login screen

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,11 +1,7 @@
 import Reactotron, { networking } from 'reactotron-react-native'
 import { queryClient } from './src/utils/queryClient'
 import { QueryClientManager, reactotronReactQuery } from 'reactotron-react-query'
-// Reactotron.setAsyncStorageHandler(AsyncStorage)
-//   .configure() // controls connection & communication settings
-//   .use(networking())
-//   .useReactNative() // add all built-in react native plugins
-//   .connect() // let's connect!
+
 const queryClientManager = new QueryClientManager({
   queryClient
 })
